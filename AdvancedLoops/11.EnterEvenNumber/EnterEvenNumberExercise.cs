b0aab90c90020063d586f95f5132e5f9725a0a08
Slav Petkov <slav.petev@gmail.com>
2017-02-20 22:25:16 +0200

    Solved problems from Advanced Loops

---
+++ AdvancedLoops/11.EnterEvenNumber/EnterEvenNumberExercise.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter even number: ");
+            int number;
+            bool parseSuccessful = int.TryParse(
+               Console.ReadLine(), out number);
+
+            while (!(parseSuccessful && number % 2 == 0))
+            {
+                Console.WriteLine("Invalid number!");
+                Console.Write("Enter even number: ");
+                parseSuccessful = int.TryParse(
+                    Console.ReadLine(), out number);
+            }
+
+            Console.WriteLine("Even number entered: {0}",
+                number);
+        }
+    }
+}
